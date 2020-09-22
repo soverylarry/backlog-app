0e95df7c2718cc04dce7ba82ea9d5e6c7dae4ae1
Minty <amina.seidi@uconn.edu>
2020-09-22 19:43:22 -0400

    splitting components

--- src/App.js
+++ src/App.js
@@ -1,27 +1,13 @@
 import React from 'react';
 import './App.css';
-import { Layout, Header, Navigation, Drawer, Content} from "react-mdl"
-import { Link } from "react-router-dom"
+import { Layout, Content} from "react-mdl"
+
 
 
 function App() {
   return (
     <div className="demo-big-content">
           <Layout>
-              <Header title="Sample Title" className= "header" scroll>
-                  <Navigation>
-                      <Link to="/">Home</Link>
-                      <Link to="/movies">Movies</Link>
-                      <Link to="/books">Books</Link>
-                  </Navigation>
-              </Header>
-              <Drawer title="Title">
-                  <Navigation>
-                      <Link to="/">Home</Link>
-                      <Link to="/movies">Movies</Link>
-                      <Link to="/books">Books</Link>
-                  </Navigation>
-              </Drawer>
               <Content />
           </Layout>
     </div>
