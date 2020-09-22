bb8277d32039846e76dd57ecd14e0a04fbcf13f5
Amina-Seidi <63622254+Amina-Seidi@users.noreply.github.com>
2020-09-22 19:51:03 -0400

    Revert "Started working on setting up a test server, isolated the Sidebar com…"

--- server.js
+++ server.js
@@ -1,39 +1,26 @@
+
 const mongoose = require("mongoose");
-const express = require("express");
-const path = require("path");
-const PORT = process.env.PORT || 3001;
-const app = express();
+const http = require("http");
 
-// Define middleware here
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+// Define a port to listen for incoming requests
+const PORT = 8080;
 
-// Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+// Create a generic function to handle requests and responses
+function handleRequest(request, response) {
+  response.end("It Works!! Path Hit: " + request.url);
 }
 
+// Use the Node HTTP package to create our server.
+// Pass the handleRequest function to empower it with functionality.
+const server = http.createServer(handleRequest);
+
+//Establish Mongoose connection
 mongoose.connect(
   process.env.MONGODB_URI || "mongodb://localhost/backlogdb",
   { useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true }
 );
 
-// Define API routes here
-
-
-// Send every other request to the React app
-// Define any API routes before this runs
-
-/*app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});*/
-
-app.get("/apitest", (req, res) => {
-  res.sendFile(path.join(__dirname, "./testing/testapi.html"));
-})
-
-app.listen(PORT, () => {
-  console.log(`🌎 ==> API server now on port ${PORT}!`);
+// Start our server so that it can begin listening to client requests.
+server.listen(PORT, function() {
+  console.log("Server listening on: http://localhost:" + PORT);
 });
-
-
--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import './App.css';
-import { Layout, Header, Navigation, Content} from "react-mdl"
+import { Layout, Header, Navigation, Drawer, Content} from "react-mdl"
 import { Link } from "react-router-dom"
-import Sidebar from "./components/sidebar";
 
 
 function App() {
@@ -16,7 +15,13 @@ function App() {
                       <Link to="/books">Books</Link>
                   </Navigation>
               </Header>
-              <Sidebar />
+              <Drawer title="Title">
+                  <Navigation>
+                      <Link to="/">Home</Link>
+                      <Link to="/movies">Movies</Link>
+                      <Link to="/books">Books</Link>
+                  </Navigation>
+              </Drawer>
               <Content />
           </Layout>
     </div>
