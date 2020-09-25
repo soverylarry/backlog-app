d1de93ae0b10dfd4d69ad2d4ae1e7f0c3dcdbb1a
KekeliVa <kekeli.afantchao@yahoo.com>
2020-09-24 20:07:51 -0400

    initial commit to card branch

--- src/App.js
+++ src/App.js
@@ -1,31 +1,36 @@
 import React from 'react';
 import './App.css';
-import { Layout, Header, Navigation, Drawer, Content} from "react-mdl"
+import { Layout, Header, Navigation, Drawer, Content } from "react-mdl"
+import Grid from "react-mdl"
+import Cell from "react-mdl"
 import { Link } from "react-router-dom"
+import MediaCard from "./components/card"
 
 
 function App() {
-  return (
-    <div className="demo-big-content">
-          <Layout>
-              <Header title="Sample Title" className= "header" scroll>
-                  <Navigation>
-                      <Link to="/">Home</Link>
-                      <Link to="/movies">Movies</Link>
-                      <Link to="/books">Books</Link>
-                  </Navigation>
-              </Header>
-              <Drawer title="Title">
-                  <Navigation>
-                      <Link to="/">Home</Link>
-                      <Link to="/movies">Movies</Link>
-                      <Link to="/books">Books</Link>
-                  </Navigation>
-              </Drawer>
-              <Content />
-          </Layout>
-    </div>
-  );
+    return (
+        <div className="demo-big-content">
+            <Layout>
+                <Header title="Sample Title" className="header" scroll>
+                    <Navigation>
+                        <Link to="/">Home</Link>
+                        <Link to="/movies">Movies</Link>
+                        <Link to="/books">Books</Link>
+                    </Navigation>
+                </Header>
+                <Drawer title="Title">
+                    <Navigation>
+                        <Link to="/">Home</Link>
+                        <Link to="/movies">Movies</Link>
+                        <Link to="/books">Books</Link>
+                    </Navigation>
+                </Drawer>
+                <Content>
+                    <MediaCard />
+                </Content>
+            </Layout>
+        </div>
+    );
 }
 
-export default App;
+export default App; 
---
+++ src/components/card.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react"
+
+
+export default class MediaCard extends Component {
+  render() {
+    return (
+      <div className="project-cards">
+        <div className="card">
+          <div className="container">
+            <h4>Testing</h4>
+            <p>Testing</p>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
