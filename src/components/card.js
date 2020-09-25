e75964f42252e122fe5a6ae9aec9afd46419d903
KekeliVa <kekeli.afantchao@yahoo.com>
2020-09-24 20:29:54 -0400

    added styling to card and placeholders for desired data

--- src/components/card.js
+++ src/components/card.js
@@ -4,13 +4,11 @@ import React, { Component } from "react"
 export default class MediaCard extends Component {
   render() {
     return (
-      <div className="project-cards">
-        <div className="card">
-          <div className="container">
-            <h4>Testing</h4>
-            <p>Testing</p>
-          </div>
-        </div>
+      <div className="project-cards" style={{width: "50%"}}>
+            <h4>Title</h4>
+            <p>Status</p>
+            <p>Type</p>
+            <input placeholder="comment"></input>
       </div>
     )
   }
