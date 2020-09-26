67a20d5f09fc71144cd76f59b388395c71b50997
njohnerwin <njohnerwin@gmail.com>
2020-09-26 13:14:13 -0400

    Resolved merge conflict

--- src/App.js
+++ src/App.js
@@ -1,20 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+<<<<<<< HEAD
 import { Layout, Header, Navigation, Drawer, Content } from "react-mdl"
 import Grid from "react-mdl"
 import Cell from "react-mdl"
+=======
+import { Layout, Header, Navigation, Drawer, Content} from "react-mdl"
+>>>>>>> c0f458b... App.js changes
 import { Link } from "react-router-dom"
 import MediaCard from "./components/card"
 import API from "./utils/API";
+<<<<<<< HEAD
 //import CreateNewmedia from "./components/createnewmedia";
+=======
+//import CreateNewMedia from "./components/createnewmedia";
+>>>>>>> c0f458b... App.js changes
 
 function App() {
   const [mediaList, setMediaList] = useState([]);
 
-    const getFullMediaList = event => {
-        API.getFullMediaList()
-            .then(res => setMediaList(res.data))
-            .catch(err => console.log(err));
+    const getFullMediaList = () => {
+      API.getFullMediaList()
+        .then(res => setMediaList(res.data))
+        .catch(err => console.log(err));
+    }
+
+    const printMediaList = () => {
+      for (let i; i < mediaList.length; i++) {
+        
+      }
     }
 
     //POST route
@@ -25,6 +39,7 @@ function App() {
   return (
     <div className="demo-big-content">
           <Layout>
+<<<<<<< HEAD
               <Header title="Sample Title" className= "header" scroll>
                   <Navigation>
                       <Link to="/">Home</Link>
@@ -41,6 +56,25 @@ function App() {
                </Drawer>
               <Content />
     {/* <CreateNewmedia /> */}
+=======
+            <Header title="Sample Title" className= "header" scroll>
+              <Navigation>
+                <Link to="/">Home</Link>
+                <Link to="/movies">Movies</Link>
+                <Link to="/books">Books</Link>
+              </Navigation>
+            </Header>
+            <Drawer title="Title">
+              
+              <Navigation>
+                <Link to="/">Home</Link>
+                <Link to="/movies">Movies</Link>
+                <Link to="/books">Books</Link>
+              </Navigation>
+            </Drawer>
+            <Content />
+            {/* <CreateNewMedia /> */}
+>>>>>>> c0f458b... App.js changes
           </Layout>
     </div>
   );
