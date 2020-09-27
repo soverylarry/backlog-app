e587173dafe7633e4952c572241b734d46101c8e
njohnerwin <njohnerwin@gmail.com>
2020-09-26 20:10:48 -0400

    Finished the initial -get- dispaly loop

--- src/App.js
+++ src/App.js
@@ -1,20 +1,12 @@
 import React, { useState } from 'react';
 import './App.css';
-<<<<<<< HEAD
 import { Layout, Header, Navigation, Drawer, Content } from "react-mdl"
 import Grid from "react-mdl"
 import Cell from "react-mdl"
-=======
-import { Layout, Header, Navigation, Drawer, Content} from "react-mdl"
->>>>>>> c0f458b... App.js changes
 import { Link } from "react-router-dom"
 import MediaCard from "./components/card"
 import API from "./utils/API";
-<<<<<<< HEAD
-//import CreateNewmedia from "./components/createnewmedia";
-=======
 //import CreateNewMedia from "./components/createnewmedia";
->>>>>>> c0f458b... App.js changes
 
 function App() {
   const [mediaList, setMediaList] = useState([]);
@@ -25,11 +17,12 @@ function App() {
         .catch(err => console.log(err));
     }
 
-    const printMediaList = () => {
-      for (let i; i < mediaList.length; i++) {
-        
-      }
-    }
+    getFullMediaList();
+    console.log(mediaList);
+
+    /*const printMediaList = (array) => {
+      
+    }*/
 
     //POST route
     /*const addMediaToList = event => {
@@ -39,24 +32,6 @@ function App() {
   return (
     <div className="demo-big-content">
           <Layout>
-<<<<<<< HEAD
-              <Header title="Sample Title" className= "header" scroll>
-                  <Navigation>
-                      <Link to="/">Home</Link>
-                      <Link to="/movies">Movies</Link>
-                      <Link to="/books">Books</Link>
-                  </Navigation>
-              </Header>
-              <Drawer title="Title">
-                  <Navigation>
-                      <Link to="/">Home</Link>
-                      <Link to="/movies">Movies</Link>
-                      <Link to="/books">Books</Link>
-                  </Navigation>
-               </Drawer>
-              <Content />
-    {/* <CreateNewmedia /> */}
-=======
             <Header title="Sample Title" className= "header" scroll>
               <Navigation>
                 <Link to="/">Home</Link>
@@ -65,16 +40,18 @@ function App() {
               </Navigation>
             </Header>
             <Drawer title="Title">
-              
               <Navigation>
                 <Link to="/">Home</Link>
                 <Link to="/movies">Movies</Link>
                 <Link to="/books">Books</Link>
               </Navigation>
             </Drawer>
-            <Content />
+            <Content>
+              {mediaList.map(media => (
+                <MediaCard />
+              ))}
+            </Content>
             {/* <CreateNewMedia /> */}
->>>>>>> c0f458b... App.js changes
           </Layout>
     </div>
   );
